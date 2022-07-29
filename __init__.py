7ef5754c801ae4f5d1198d547513c7e87aae804f
Aneesh Naik <aneesh92@hotmail.co.uk>
2022-07-29 17:43:54 +0200

    Add README, LICENSE and __init__

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+init file of batchgauss.
+
+Created: July 2022
+Author: A. P. Naik
+"""
+from .batchgauss import sample
+
+__all__ = ['sample']
